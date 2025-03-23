f15881171dd56ccffd265d2bcf3c215ec13bff9f
Andreeva-Regina-E <andreeva.regina.e@gmail.com>
2025-03-23 19:23:04 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,21 @@ LOGImg.addEventListener('mouseenter', () => {
 });
 
 });
+
+
+
+const heightHeader = header.offsetHeight;           // определяем высоту блока, включая внутренние отступы
+
+document.addEventListener('scroll', () => {         // навешиваем слушатель событий на scroll страницы и ожидаем ее прокрутку
+
+    console.log('Страница скролится');
+
+    let scrollPageY = this.scrollY;                 // получаем значение насколько прокрутили страницу
+
+    if (scrollPageY > heightHeader) {               // условие: если расстояние от верха страницы больше высоты элемента
+        header.classList.add('header--scroll')      // устанавливаем класс модификатора на элемент
+    } else {
+        header.classList.remove('header--scroll')   // удаляем класс модификатора у элемента
+    }
+
+});
